9848272810ea5ba917edacc04d6a94a20d870d73
rlander@microsoft.com <Rich Lander>
2024-01-08 15:59:22 -0800

    Add projects

---
+++ projects/grpc-client/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using grpc;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
